261457be0b5657d588c9f9ac68c990036137bad6
sun <sun@unleashedmind.com>
2008-10-25 13:35:51 +0000

    #325980 by markus_petrux: Added Spanish/Catalan translation for Break plugin.

---
+++ plugins/break/langs/ca.js
@@ -0,0 +1,7 @@
+// $Id$
+
+tinyMCE.addToLang('break', {
+  title: 'Inserir marcador de document retallat',
+  desc: 'Generar el punt de separació entre la versió retallada del document i la resta del contingut'
+});
+
---
+++ plugins/break/langs/es.js
@@ -0,0 +1,7 @@
+// $Id$
+
+tinyMCE.addToLang('break', {
+  title: 'Insertar marcador de documento recortado',
+  desc: 'Generar el punto de separación entre la versión recortada del documento y el resto del contenido'
+});
+
